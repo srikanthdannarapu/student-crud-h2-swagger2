12c0769612a369614a3a6ba27683263ec0501005
Srikanth Dannarapu <srikant.dannarapu@gmail.com>
2019-12-13 20:57:37 +0530

    initial commit

---
+++ src/main/java/com/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.exception;
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(Exception.class)
+  public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) {
+    ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(),
+        request.getDescription(false));
+    return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(StudentNotFoundException.class)
+  public final ResponseEntity<ErrorDetails> handleUserNotFoundException(StudentNotFoundException ex, WebRequest request) {
+    ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(),
+        request.getDescription(false));
+    return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+  }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into student
+values(10001,'Srikanth', 'E1234567');
+
+insert into student
+values(10002,'Dannarapu', 'A1234568');
